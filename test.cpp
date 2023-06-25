7ef597a3641ff165b37775aa6d2efcc84e00b8fd
Philipp1343 <phabti@gmail.com>
2023-06-25 20:08:28 +0200

    hello :)

--- test.cpp
+++ test.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 
 void helloWorld(){
-std::cout << "Hello World!";
+std::cout << "Hello";
 };
 
 
 int main() {
     helloWorld();
-    int num = 20;
-    std::cout << "/n meow meow meow meow meow meow meow meow meow";  
+    
     return 0;
 }
