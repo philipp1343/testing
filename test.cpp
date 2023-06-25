399724d6ae2424be1e75d4dec1f200bff94f6571
Philipp1343 <phabti@gmail.com>
2023-06-25 20:04:53 +0200

    20

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ std::cout << "Hello World!";
 
 int main() {
     helloWorld();
-    int num = 23;
+    int num = 20;
     std::cout << "/n meow meow meow meow meow meow meow meow meow";  
     return 0;
 }
